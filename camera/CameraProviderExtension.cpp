a0723e5c0e0ee26b4e10b54e459ccddd66eef64a
John Galt <johngaltfirstrun@gmail.com>
2026-05-19 20:36:09 +0000

    sm8450-common: camera: add enabled bool for torch workarounds and toggle switch
    Co-authored-by: ShadeDark6 <nizamfa6@gmail.com>
    Change-Id: Ie350a6cdeed51d50450fe2ca7da812f49d38385d

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -6,11 +6,11 @@
 #define LOG_TAG "CameraProviderExtension"
 
 #include "CameraProviderExtension.h"
-#include <android-base/file.h>
 #include <android-base/logging.h>
+#include <fstream>
+#include <string>
 
-using ::android::base::ReadFileToString;
-using ::android::base::WriteStringToFile;
+#define TOGGLE_SWITCH "/sys/class/leds/led:switch_2/brightness"
 
 static const std::string kTorchBrightnessNodes[] = {
         "/sys/class/leds/led:torch_0/brightness",
@@ -18,6 +18,34 @@ static const std::string kTorchBrightnessNodes[] = {
         "/sys/class/leds/led:torch_2/brightness",
         "/sys/class/leds/led:torch_3/brightness",
 };
+
+/**
+ * Write value to path and close file.
+ */
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    if (file.is_open()) {
+        file << value;
+    } else {
+        LOG(ERROR) << "Failed to open node for writing: " << path;
+    }
+}
+
+/**
+ * Read value from the path and close file.
+ */
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+    if (!file.is_open()) {
+        LOG(ERROR) << "Failed to open node for reading: " << path;
+        return def;
+    }
+    file >> result;
+    return file.fail() ? def : result;
+}
 
 bool supportsTorchStrengthControlExt() {
     return true;
@@ -38,25 +66,21 @@ int32_t getTorchMaxStrengthLevelExt() {
 
 int32_t getTorchStrengthLevelExt() {
     // We write the same value in all the LEDs, so get from the first one.
-    auto node = kTorchBrightnessNodes[0];
-    if (std::string value; ReadFileToString(node, &value, true))
-        return std::stoi(value);
-    else
-        LOG(ERROR) << "Failed to read from node: " << node;
-
-    return getTorchDefaultStrengthLevelExt();
+    return get(kTorchBrightnessNodes[0], 0);
 }
 
-void setTorchStrengthLevelExt(int32_t torchStrength) {
-    LOG(DEBUG) << "setTorchStrengthLevelExt(" << torchStrength << ")";
-    auto value = std::to_string(torchStrength);
-    for (auto& node : kTorchBrightnessNodes) {
-        if (!WriteStringToFile(value, node, true))
-            LOG(ERROR) << "Failed writing value " << value << " to node: " << node;
+void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
+    LOG(DEBUG) << "setTorchStrengthLevelExt(" << torchStrength << ", " << enabled << ")";
+    set(TOGGLE_SWITCH, 0);
+    for (const auto& node : kTorchBrightnessNodes) {
+        set(node, torchStrength);
+    }
+    if (enabled) {
+        set(TOGGLE_SWITCH, 255);
     }
 }
 
 void setTorchModeExt(bool enabled) {
     LOG(DEBUG) << "setTorchModeExt(" << enabled << ")";
-    // noop
+    setTorchStrengthLevelExt(enabled ? getTorchDefaultStrengthLevelExt() : 0, enabled);
 }
